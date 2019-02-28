b89c60d59e4e671ce5d79d93bdd0f9e7a856d9d2
Lebogang <lebothipenyane@gmail.com>
2019-02-28 10:40:02 +0200

    DashBoardChanges

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -59,27 +59,27 @@ export class AppComponent {
     this.chart = new Chart("Overall", {
       type: 'doughnut',
       data: {
-        labels: ["Orphanage", "Disability", "Old Age Homes", "Theraphy", "Psychiatric centres/Hospital", "Social Centre", "Rehab"],
+        labels: ["Disability", "Orphanage", "Psychiatric centres/Hospital", "Rehab", "Old Age Homes", "Social Centre", "Theraphy"],
         datasets: [{
           label: 'Number of Orgs',
-          data: [this.orph, this.disablty, this.oldAge, this.theraphy, this.Psychiatric, this.sCenter, this.Rehab],
+          data: [this.disablty, this.orph, this.Psychiatric, this.Rehab, this.oldAge, this.sCenter, this.theraphy],
           backgroundColor: [
-            'rgba(255, 99, 132, 1)',
             'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
+            'rgba(255, 99, 132, 1)',
             'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 40, 1)',
             'rgba(20, 162, 70, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(255, 159, 64, 1)',
+            'rgba(75, 192, 192, 1)',
           ],
           borderColor: [
-            'rgba(255,99,132,1)',
             'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
+            'rgba(255, 99, 132, 1)',
             'rgba(153, 102, 255, 1)',
+            'rgba(20, 162, 70, 1)',
+            'rgba(255, 206, 86, 1)',
             'rgba(255, 159, 64, 1)',
-            'rgba(54, 162, 235, 1)',
+            'rgba(75, 192, 192, 1)',
           ],
           borderWidth: 1
         }]
